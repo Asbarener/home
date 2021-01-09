77c191595e7dc14f2a8d4ee50d3bfb8260ff06c2
Asbar <asbarener@outlook.com>
2021-01-09 23:56:29 +0800

    i3

--- scripts/feh.sh
+++ scripts/feh.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #while true; do
 	# ~/mywallpapers/T5是我存放壁纸的文件夹，换成你自己的
-feh --bg-fill ~/Pictures/favorite.png
-	# 每15秒自动切换壁纸
+feh --bg-fill ~/Pictures/project/15.jpg
+# 每15秒自动切换壁纸
 #	sleep 15
 #done
---
+++ scripts/random.sh
@@ -0,0 +1,1 @@
+feh --bg-fill --no-fehbg --randomize ~/Pictures/project 
